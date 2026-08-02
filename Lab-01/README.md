27297d83549924c7aa3da45c8dc0bb6fbb8262e1
yadev07 <yadevsingh.nishad@gmail.com>
2026-08-02 22:40:37 +0530

    LAb 01 Submission

---
+++ Lab-01/README.md
@@ -0,0 +1,12 @@
+# Student Name : Yadev Singh Nishad
+# Scholar Number : 23145026
+# Course : BCA 
+# Semester : VII
+# Subject : Node.js
+# Lab Number : 01
+# Date : 31/07/2026
+# Brief description of the practical :
+
+/*
+In this practical, I learned the basics of Node.js by installing and configuring it on my system. I verified the installation using Node.js and NPM commands, created my first Node.js project, and initialized it using npm. I also wrote simple programs using console.log(), worked with variables and different data types, and understood the key differences between Browser JavaScript and Node.js. Finally, I learned how to organize a Node.js project and upload it to GitHub for version control and submission.
+*/
---
+++ Lab-01/app.js
@@ -0,0 +1,35 @@
+const name = 'Yadev Singh Nishad';
+const scholar_number = 23145026;
+const course = 'BCA';
+let semester = 'VII';
+const college = 'DSVV';
+
+console.log('Welcome to Node.js');
+console.log("Name : ", name);
+console.log("Scholar Number : ", scholar_number);
+console.log("Course : ", course);
+console.log("Semester : ", semester);
+console.log("College : ", college);
+
+console.log("Hello Node.js");
+console.log("Learning Backend Development");
+console.log("Today's Lab Completed Successfully");
+
+
+
+let myString = "Hello";
+
+let myNumber = 100;
+
+let myBoolean = true;
+
+let myUndefined;
+
+let myNull = null;
+
+// Displaying data types
+console.log(typeof myString);      
+console.log(typeof myNumber);      
+console.log(typeof myBoolean);     
+console.log(typeof myUndefined);   
+console.log(typeof myNull);        
